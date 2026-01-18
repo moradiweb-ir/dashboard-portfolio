d00479c332c857f5beccb23df9929f09b0589cfe
moradiweb <moradiweb.ir@gmail.com>
2026-01-19 00:29:45 +0330

    Build item sidebar.tsx

--- app/components/Sidebar.tsx
+++ app/components/Sidebar.tsx
@@ -1,9 +1,33 @@
-
+import Image from "next/image";
 
 function Sidebar() {
   return (
-    <div className="flex w-sm bg-blue-500">Sidebar</div>
-  )
+    <div className="flex flex-col w-62.5 h-lvh bg-blue-100">
+      <div className="flex ml-4 gap-3 p-4">
+        <Image src={"/favicon.ico"} width={20} height={20} alt={""} />
+        <p className="text-black">Admin Dashboard</p>
+      </div>
+      <div className="flex w-full bg-gray-400 h-px"></div>
+      <div className=" flex-col mt-8 ml-8 ">
+        <div className="flex gap-4 mr-5  p-3 rounded-sm">
+          <Image src={"/favicon.ico"} width={20} height={20} alt={""} />
+          <p className="text-gray-700 mr-4 ">Dashboard</p>
+        </div>
+        <div className="flex gap-4 mt-2 mr-5 p-3 rounded-sm ">
+          <Image src={"/favicon.ico"} width={20} height={20} alt={""} />
+          <p className="text-gray-700 mr-4 ">Analytics</p>
+        </div>
+        <div className="flex gap-4 mt-2 mr-5  p-3 rounded-sm">
+          <Image src={"/favicon.ico"} width={20} height={20} alt={""} />
+          <p className="text-gray-700 mr-4 ">Tasks</p>
+        </div>
+        <div className="flex gap-4 mt-2 mr-5  p-3 rounded-sm">
+          <Image src={"/favicon.ico"} width={20} height={20} alt={""} />
+          <p className="text-gray-700 mr-4 ">Setting</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Sidebar
+export default Sidebar;
