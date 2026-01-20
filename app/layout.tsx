b0b34085a579260bffe195060cb0f47cf114bded
moradiweb <moradiweb.ir@gmail.com>
2026-01-20 17:37:38 +0330

    fixed bug padding in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,10 @@
 import type { Metadata } from "next";
-
 import "./globals.css";
 import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
-
 export const metadata: Metadata = {
   title: "Dashboard-Portfolio",
 };
-
 export default function RootLayout({
   children,
 }: {
@@ -15,17 +12,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+
       <body>
-        <div className="flex h-screen w-full">
-          <div className=" pl-5 pt-5">
-            <Sidebar />
-          </div>
-
+        
+        <div className="flex h-screen w-full p-5">
+           <Sidebar />
           <div className="flex flex-col flex-1">
-            <div className="pt-5 pr-5">
-              <Header />
-            </div>
-
+           <Header />
             <main className="flex-1 px-5 pb-5 overflow-y-auto">{children}</main>
           </div>
         </div>
